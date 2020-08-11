30536649c20f6a9a915a6e2bcfaeec8e0643c3b2
RomanMereneanu <asasin84@mail.ru>
2020-08-11 23:20:43 +0300

    Add Home page all

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,12 +13,14 @@
     </ul>
     <h3>Essential Links</h3>
     <ul>
+      
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
+
     <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
@@ -32,7 +34,7 @@
 
 <script>
 export default {
-  name: 'Add New fonts',
+  name: 'Add Home page all',
   props: {
     msg: String
   }
